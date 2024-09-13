e13c591c9862b682ca5315bf49184eecfbebe6c1
Nikolai <19nikolai92@gmail.com>
2024-09-13 14:11:42 +0300

    upd

--- app/Http/Resources/CommentsResource.php
+++ app/Http/Resources/CommentsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,7 +13,7 @@ class CommentsResource extends JsonResource
 		return [
             'id' => $this->id,
             'content' => $this->content,
-            'created_at' => $this->created_at,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
 		];
 	}
 }
